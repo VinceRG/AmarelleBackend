1b96eb9e7de212e399c6477f743008b3349a03dc
VinceRG <vincegonato48@gmail.com>
2025-12-05 14:21:57 +0800

    Backend

--- python/app.py
+++ python/app.py
@@ -396,6 +396,7 @@ def analytics():
             "hip_circ_cm": avg_hips
         }
     })
-
+    
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
